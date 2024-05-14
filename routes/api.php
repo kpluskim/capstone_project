5acd68c7e727bb7de59df3200cd0bb0aa2f97eab
Michael Lazaro <yuna5566@gmail.com>
2024-05-14 18:27:00 +0800

    chore: prep for next js

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,12 @@ use App\Http\Controllers\CarController;
 use App\Http\Controllers\DriverController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 // Authentication
 Route::post('/authenticate', [UserController::class,'authenticate']);
@@ -47,5 +41,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/transactions/transaction_create', [TransactionController::class,'transaction_create'] );
     Route::put('/transactions/transaction_update/{id}', [TransactionController::class,'transaction_update'] );
 });
-
-
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\DriverController;
-use App\Http\Controllers\BookingController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
+
+require __DIR__.'/auth.php';
